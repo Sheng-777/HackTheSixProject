7e3d34afbf23fd039c1e47a3b8510c0282cd5591
Sheng-777 <142447604+Sheng-777@users.noreply.github.com>
2023-08-17 21:09:01 +0000

    update webscrape

--- .vscode/WebScrapeTest.py
+++ .vscode/WebScrapeTest.py
@@ -29,27 +29,57 @@ matches_id = req_matchesId.json()
 # print(matches_id)
 
 # get detail match info
+recent_win = 0
+recent_lose = 0
+streak = 0
+on_streak = True
 
 for matchId in matches_id:
     api_url_matchInfo = "https://americas.api.riotgames.com/lol/match/v5/matches/"+matchId+"?api_key="+api_key
     req_matchInfo = requests.get(api_url_matchInfo)
     matchInfo = req_matchInfo.json()
-    #print(matchInfo)
-    
+    #print(matchInfo["info"]["teams"][0])
+    #print(matchInfo["info"]["teams"][0])
+
     participants = matchInfo["info"]["participants"]
-    #print (participants)
+    #print(participants)
     
     
-    for participant in participants:
+    for i in range(len(participants)):
+        #break    
+        summonerName = participants[i]["summonerName"]
+        champion = participants[i]["championName"]
+        kills = participants[i]["kills"]
+        deaths = participants[i]["deaths"]
+        assists = participants[i]["assists"]
+        win = participants[i]["win"]        
         
-        #print(participant)
+        if summonerName == player_name:
+            if win:
+                recent_win += 1
+                if on_streak:
+                    if streak >= 0:
+                        streak += 1
+                    else:
+                        on_streak = False
+                    
+                    
+            else:
+                recent_lose += 1
+                if on_streak:
+                    if streak <= 0:
+                        streak -=1
+                    else:
+                        on_streak = False
         
-        summonerName = participant["summonerName"]
-        champion = participant["championName"]
-        kills = participant["kills"]
-        deaths = participant["deaths"]
-        assists = participant["assists"]
-        
-        print(f"summoner name: {summonerName} | champions: {champion} | kills: {kills} | deaths: {deaths} | assists: {assists}")
-        
+        print(f"    summoner name: {summonerName} | champions: {champion} | kills: {kills} | deaths: {deaths} | assists: {assists} | win: {win}")
+    
+    
+    #break
     print("--------------------------------")
+
+print("Summary:")
+
+print(f"recent streak: {streak}")
+print(f"recent win: {recent_win}")
+print(f"recent lose: {recent_lose}")
