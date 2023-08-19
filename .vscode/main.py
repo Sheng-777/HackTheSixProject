8cafa34f525ce30971ff9a5d7f19e09795c716f0
Alex <alex.you0825@gmail.com>
2023-08-19 19:37:21 -0400

    added icon fixed search bar

--- .vscode/main.py
+++ .vscode/main.py
@@ -10,31 +10,27 @@ class MyGUI(QMainWindow):
         super(MyGUI, self).__init__()
         p = Path("adcdiffgg.ui").resolve()
         uic.loadUi(str(p),self)
-        #uic.loadUi(r"C:\Users\alexj\OneDrive\Documents\GitHub\HackTheSixProject\adcdiffgg.ui", self)
-        #uic.loadUi("adcdiffgg.ui", self)
-        #screen_resolution = application.desktop().screenGeometry()
-        #width, height = screen_resolution.width(), screen_resolution.height()
         
-        #self.setGeometry(int(width/2 - WinW/2), int(height/2 - WinH/2))
         self.setWindowTitle("ADCDIFF.gg")
         self.setToolTip("ADCdiff.gg")
+        
+        p = Path("adcdifficon.png").resolve()
+        self.setWindowIcon(QIcon(str(p)))
         
         # create logo image
         self.label = QLabel(self)
         # loading image
         p = os.path.abspath("adcdifflogo.png")
         self.pixmap = QPixmap(str(p))
-        #self.pixmap = QPixmap(r'C:\Users\alexj\OneDrive\Documents\GitHub\HackTheSixProject\adcdifflogo.png')
-        #self.pixmap = QPixmap('adcdifflogo.png')
-        self.pixmap = self.pixmap.scaled(400,200)
+        self.pixmap = self.pixmap.scaled(400,260)
         # adding image to label
         self.label.setPixmap(self.pixmap)
         # Optional, resize label to image size
         self.label.resize(self.pixmap.width(),
                           self.pixmap.height())
-        self.label.move(300,20)       
-
+        self.label.move(300,0)       
         self.show()
+        self.enterSumm.returnPressed.connect(self.enter)
         self.go.clicked.connect(self.clicked)
 
     def clicked(self):
@@ -42,11 +38,18 @@ class MyGUI(QMainWindow):
         region = self.enterReg.currentText()
         if summText == "":
             message = QMessageBox()
+            message.setWindowTitle("Invalid operation")
             message.setText("Please enter a Summoner name")
             message.exec_()
         else:
             print('Summoner name: ' + summText)
             print('Region: ' + region)
+        
+    def enter(self):
+        summText = self.enterSumm.text()
+        region = self.enterReg.currentText()
+        print('Summoner name: ' + summText)
+        print('Region: ' + region)
         
 def main():
     app = QApplication([])
